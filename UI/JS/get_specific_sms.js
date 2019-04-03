170c480bf0b743e27edca1ad76654ce09ac5c1ba
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2019-04-03 12:09:35 +0300

    Impementing feedback

--- UI/JS/get_specific_sms.js
+++ UI/JS/get_specific_sms.js
@@ -19,14 +19,14 @@ const getSpecificMessage =(message_id) =>{
             outPutData = `<h2>My Messages</h2><hr>`
             outPutData +=
             `<div>
-            <h3>${message.data.subject}</h3>
-            <div>
+            
+            <h3>RE: ${message.data.subject}<div id="sender_email">
+            Sent by: ${message.data.sender_email}
+            </div></h3>
+            <div id="messo">
                 ${message.data.message}
             </div>
-            <div id="sender_email">
-                Sent by: ${message.data.sender_email}
-            </div>
-            <div class="button">
+            <div class="button" id="button_del">
                 <button type="submit" class="save_button"onclick="deleteSpecificMessage(${message.data.message_id})">Delete</button>
             </div>
             </div>
@@ -55,14 +55,14 @@ const getSpecificMessageInSentBox =(message_id) =>{
             outPutData = `<h2>Sent Messages</h2>`
             outPutData +=
             `<div>
-            <h3>${message.data.subject}</h3>
-            <div>
+            <h3>RE: ${message.data.subject}<div id="receiver_email">
+            Sent to: ${message.data.receiver_email}
+            </div></h3>
+            <div id="messo">
                 ${message.data.message}
             </div>
-            <div id="receiver_email">
-                Sent to: ${message.data.receiver_email}
-            </div>
-            <div class="button">
+            
+            <div class="button" id="button_del">
                 <button type="submit" class="save_button"onclick="deleteSpecificMessage(${message.data.message_id})">Delete</button>
             </div>
             </div>
